f1dcbbcda4e4d429eb26e6b8135a6dae318b50a9
Stephen <stephennguyen24@gmail.com>
2024-11-22 03:19:53 +0700

    feat: update UI

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,7 @@
 const copySVG =
-  '<svg height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512.002 512.002" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <polygon style="fill:#CFF09E;" points="174.475,131.664 211.409,131.664 211.409,16.977 15.593,16.977 15.593,380.336 174.475,380.336 "></polygon> <polygon style="fill:#CFF09E;" points="373.389,131.664 370.291,131.664 370.291,246.349 494.324,246.349 "></polygon> </g> <path style="fill:#507C5C;" d="M496.407,309.978c-8.61,0-15.593,6.981-15.593,15.593v153.857H190.068v-99.092V147.257h21.342 h124.033H354.7v99.092c0,8.612,6.982,15.593,15.593,15.593h124.034c6.382,0,12.119-3.889,14.484-9.817 c2.364-5.928,0.876-12.699-3.755-17.09L384.121,120.35c-0.128-0.122-0.267-0.226-0.398-0.343c-0.192-0.17-0.384-0.341-0.583-0.504 c-0.186-0.148-0.376-0.285-0.568-0.424c-0.214-0.156-0.426-0.313-0.649-0.458c-0.184-0.12-0.374-0.229-0.563-0.341 c-0.242-0.145-0.482-0.288-0.733-0.421c-0.178-0.094-0.36-0.175-0.541-0.26c-0.271-0.129-0.541-0.259-0.82-0.373 c-0.173-0.07-0.351-0.128-0.525-0.193c-0.293-0.108-0.583-0.215-0.883-0.304c-0.187-0.056-0.377-0.097-0.568-0.147 c-0.293-0.075-0.583-0.154-0.881-0.214c-0.245-0.048-0.493-0.076-0.741-0.114c-0.249-0.036-0.494-0.083-0.747-0.108 c-0.507-0.05-1.018-0.078-1.531-0.078h-3.098h-28.631L225.236,5.665c-2.896-2.747-6.736-4.279-10.729-4.279h-3.098l0,0l0,0H15.593 C6.982,1.386,0,8.367,0,16.979v363.359c0,8.612,6.982,15.593,15.593,15.593h143.29v99.092c0,8.612,6.982,15.593,15.593,15.593 h321.933c8.61,0,15.593-6.981,15.593-15.593V325.571C512,316.959,505.019,309.978,496.407,309.978z M385.884,230.756v-65.755 l69.34,65.755H385.884z M227.001,50.316l69.338,65.755h-69.338V50.316z M31.186,364.743V32.572h164.629l0.002,83.499h-21.342 c-8.61,0-15.593,6.981-15.593,15.593v233.079H31.186z"></path> </g></svg>';
+  '<svg height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512.002 512.002" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <polygon style="fill:#9ee5f0;" points="174.475,131.664 211.409,131.664 211.409,16.977 15.593,16.977 15.593,380.336 174.475,380.336 "></polygon> <polygon style="fill:#9ee5f0;" points="373.389,131.664 370.291,131.664 370.291,246.349 494.324,246.349 "></polygon> </g> <path style="fill:#507C5C;" d="M496.407,309.978c-8.61,0-15.593,6.981-15.593,15.593v153.857H190.068v-99.092V147.257h21.342 h124.033H354.7v99.092c0,8.612,6.982,15.593,15.593,15.593h124.034c6.382,0,12.119-3.889,14.484-9.817 c2.364-5.928,0.876-12.699-3.755-17.09L384.121,120.35c-0.128-0.122-0.267-0.226-0.398-0.343c-0.192-0.17-0.384-0.341-0.583-0.504 c-0.186-0.148-0.376-0.285-0.568-0.424c-0.214-0.156-0.426-0.313-0.649-0.458c-0.184-0.12-0.374-0.229-0.563-0.341 c-0.242-0.145-0.482-0.288-0.733-0.421c-0.178-0.094-0.36-0.175-0.541-0.26c-0.271-0.129-0.541-0.259-0.82-0.373 c-0.173-0.07-0.351-0.128-0.525-0.193c-0.293-0.108-0.583-0.215-0.883-0.304c-0.187-0.056-0.377-0.097-0.568-0.147 c-0.293-0.075-0.583-0.154-0.881-0.214c-0.245-0.048-0.493-0.076-0.741-0.114c-0.249-0.036-0.494-0.083-0.747-0.108 c-0.507-0.05-1.018-0.078-1.531-0.078h-3.098h-28.631L225.236,5.665c-2.896-2.747-6.736-4.279-10.729-4.279h-3.098l0,0l0,0H15.593 C6.982,1.386,0,8.367,0,16.979v363.359c0,8.612,6.982,15.593,15.593,15.593h143.29v99.092c0,8.612,6.982,15.593,15.593,15.593 h321.933c8.61,0,15.593-6.981,15.593-15.593V325.571C512,316.959,505.019,309.978,496.407,309.978z M385.884,230.756v-65.755 l69.34,65.755H385.884z M227.001,50.316l69.338,65.755h-69.338V50.316z M31.186,364.743V32.572h164.629l0.002,83.499h-21.342 c-8.61,0-15.593,6.981-15.593,15.593v233.079H31.186z"></path> </g></svg>';
+const logoSVG =
+  '<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#ffffff" d="M50 0c-27.614 0-50 22.386-50 50s22.386 50 50 50 50-22.386 50-50-22.386-50-50-50z"></path><path fill="#37A18E" d="M79.222 72.675c-1.415-1.415-3.45-1.802-5.225-1.199l-4.722-4.723c3.365-4.424 5.367-9.942 5.367-15.931 0-14.546-11.792-26.338-26.339-26.338-14.546 0-26.338 11.792-26.338 26.338 0 14.547 11.792 26.339 26.338 26.339 5.988 0 11.507-2.002 15.931-5.368l4.723 4.723c-.602 1.773-.215 3.81 1.199 5.224l8.923 8.923c3.492-2.502 6.655-5.435 9.396-8.733l-9.253-9.255z"></path><path fill="#F5F5F5" d="M68.535 63.464l-5.07 5.071 19.255 19.255c1.809-1.567 3.503-3.263 5.071-5.071l-19.256-19.255z"></path><path fill="#ffffff" d="M48.5 22c14.636 0 26.5 11.864 26.5 26.5 0 14.635-11.864 26.5-26.5 26.5s-26.5-11.865-26.5-26.5c0-14.636 11.864-26.5 26.5-26.5z"></path><path fill="#82CEE8" d="M48.5 27c11.874 0 21.5 9.626 21.5 21.5s-9.626 21.5-21.5 21.5-21.5-9.626-21.5-21.5 9.626-21.5 21.5-21.5z"></path><path fill="#A8DDEF" d="M38.189 53.379c-.499-.5-1.31-.5-1.81 0s-.5 1.311 0 1.811l5.431 5.43c.5.5 1.31.5 1.81 0 .5-.499.5-1.31 0-1.81l-5.431-5.431zm11.435 4.45l-10.454-10.454c-.495-.495-1.299-.495-1.794 0s-.495 1.299 0 1.794l10.454 10.454c.495.495 1.299.495 1.794 0s.495-1.298 0-1.794z"></path><path fill="#EFC75E" d="M79.606 70.485c-1.996-1.996-5.232-1.996-7.229 0l-1.892 1.892c-1.996 1.997-1.996 5.233 0 7.229l10.007 10.007c3.419-2.636 6.485-5.702 9.121-9.121l-10.007-10.007z"></path><path fill="#D7B354" d="M72.507 77.606c-1.993-1.993-1.995-5.223-.008-7.22l-.121.099-1.892 1.892c-1.996 1.997-1.996 5.233 0 7.229l10.007 10.007c.755-.582 1.491-1.187 2.211-1.81l-10.197-10.197z"></path><path fill="#72B5CC" d="M48.5 27c-6.701 0-12.684 3.067-16.627 7.873 3.71-3.044 8.454-4.873 13.627-4.873 11.874 0 21.5 9.626 21.5 21.5 0 5.173-1.829 9.917-4.873 13.627 4.806-3.943 7.873-9.926 7.873-16.627 0-11.874-9.626-21.5-21.5-21.5z"></path></g></svg>';
 
 const handleCopyToClipboard = () => {
   const buttons = document.querySelectorAll("button.copy");
@@ -61,10 +63,11 @@ const buildHTML = (info) => {
   const shopifyRedirectInfo = info.shopify.redirect;
   const boostCopyInfo = info.boost.copy;
   const boostRedirectInfo = info.boost.redirect;
-  let html = `<ul> <h1>ShopifyxBoostSD</h1>`;
+  let html = `<ul><h1>${logoSVG} ShopifyxBoostSD</h1>`;
+  html += `<hr style="margin-bottom: 20px;border-color: #7fffd436;">`;
   html += buildInfoToCopyHTML(shopifyCopyInfo);
-  html += buildInfoToRedirectHTML(shopifyRedirectInfo);
   html += buildInfoToCopyHTML(boostCopyInfo);
+  html += buildInfoToRedirectHTML(shopifyRedirectInfo);
   html += buildInfoToRedirectHTML(boostRedirectInfo);
   html += `</ul>`;
 
@@ -102,13 +105,7 @@ const constructURLLink = (location, id) => {
     redirectLinks.preview_link = currentURL.toString();
 
     currentURL.search = "";
-    if (
-      currentURL.pathname.includes("/products") ||
-      currentURL.pathname.includes("/collections")
-    ) {
-      const jsonInfoLink = currentURL.toString() + ".json";
-      redirectLinks.current_page_JSON = jsonInfoLink;
-    }
+
     return redirectLinks;
   } catch (error) {
     console.log(error);
@@ -133,8 +130,11 @@ const prepareInfo = async (data) => {
       info.shopify.copy.push(`Theme Name->${shopifyInfo[key].name}`);
     }
   }
-  info.shopify.copy.push(
-    `Other->country:${shopifyInfo.country}, currency:${shopifyInfo.currency.active}`
+
+  let shopWithoutDomain = shopifyInfo.shop.replace(".myshopify.com", "");
+
+  info.boost.redirect.push(
+    `Theme Code Editor->https://admin.shopify.com/store/${shopWithoutDomain}/themes/${shopifyInfo.theme.id}`
   );
 
   const redirectlinks = constructURLLink(location, shopifyInfo.theme.id);
@@ -155,8 +155,6 @@ const prepareInfo = async (data) => {
     versions += "" + boostVersions[key];
   }
 
-  let shopWithoutDomain = shopifyInfo.shop.replace(".myshopify.com", "");
-
   if (Object.values(boostVersions).length != 0) {
     info.boost.copy.push(`Boost Versions->${versions}`);
     info.boost.redirect.push(
@@ -170,7 +168,7 @@ const prepareInfo = async (data) => {
       shopWithoutDomain
     ).then((response) => {
       if (response) {
-        info.shopify.redirect.push(`This Page Admin->${response}`);
+        info.shopify.redirect.push(`Collection Admin->${response}`);
       }
     });
   }
